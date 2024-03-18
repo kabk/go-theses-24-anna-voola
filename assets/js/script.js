fc31f7e7b2784f3eed8449c219b0be26e57b3a78
Annavoola <annavoola@gmail.com>
2024-03-18 12:36:58 +0100

    Testrun_18march

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,27 @@
 $(document).ready(function() {
     var navbar = $(".main-nav");
-    var abstract = $("#abstract"); // Change this to target your specific abstract element
+    var jumber = $(".main-nav-jumper");
+    var windowHeight = $(window).height(); // Get the height of the viewport
     var navbarHeight = navbar.outerHeight();
+    var isSticky = false;
 
-    // Set the initial position of the navbar right below the abstract
-    navbar.css('top', abstract.offset().top + abstract.outerHeight());
+    // Set the initial position of the navbar at the bottom of the viewport
+    navbar.css('bottom', 0);
 
     $(window).scroll(function() {
-        var stickyThreshold = abstract.offset().top + abstract.outerHeight();
         var scrollTop = $(window).scrollTop();
+        var viewportBottom = windowHeight; // Calculate the bottom of the viewport
 
-        if (scrollTop >= stickyThreshold) {
-            navbar.addClass("sticky").css('top', 0); // Adjust 'top' property to align the navbar to the top
-        } else {
-            navbar.removeClass("sticky").css('top', abstract.offset().top + abstract.outerHeight());
+        if (!isSticky && scrollTop >= viewportBottom) {
+            navbar.addClass("sticky").css('bottom', 'auto').css('top', 0); // Make navbar sticky at the top
+            jumber.css('display', 'block');
+            isSticky = true;
+        } else if (isSticky && scrollTop < viewportBottom) {
+            navbar.removeClass("sticky").css('bottom', 0).css('top', 'auto'); // Return navbar to the bottom
+            jumber.css('display', 'none');
+            isSticky = false;
         }
     });
-    
 
     function toggleImage(event) {
         var btn = event.currentTarget;
