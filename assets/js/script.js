dc04389f25b5901aa061966b75c280572a70ea84
Annavoola <annavoola@gmail.com>
2024-03-25 09:14:39 +0100

    test run

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,27 @@
 $(document).ready(function() {
-    var navbar = $(".main-nav");
-    var jumber = $(".main-nav-jumper");
-    var windowHeight = $(window).height(); // Get the height of the viewport
-    var navbarHeight = navbar.outerHeight();
-    var isSticky = false;
+    // var navbar = $(".main-nav");
+    // var jumber = $(".main-nav-jumper");
+    // var windowHeight = $(window).height(); // Get the height of the viewport
+    // var navbarHeight = navbar.outerHeight();
+    // var isSticky = false;
 
-    // Set the initial position of the navbar at the bottom of the viewport
-    navbar.css('bottom', 0);
+    // // Set the initial position of the navbar at the bottom of the viewport
+    // navbar.css('bottom', 0);
 
-    $(window).scroll(function() {
-        var scrollTop = $(window).scrollTop();
-        var viewportBottom = windowHeight; // Calculate the bottom of the viewport
+    // $(window).scroll(function() {
+    //     var scrollTop = $(window).scrollTop();
+    //     var viewportBottom = windowHeight; // Calculate the bottom of the viewport
 
-        if (!isSticky && scrollTop >= viewportBottom) {
-            navbar.addClass("sticky").css('bottom', 'auto').css('top', 0); // Make navbar sticky at the top
-            jumber.css('display', 'block');
-            isSticky = true;
-        } else if (isSticky && scrollTop < viewportBottom) {
-            navbar.removeClass("sticky").css('bottom', 0).css('top', 'auto'); // Return navbar to the bottom
-            jumber.css('display', 'none');
-            isSticky = false;
-        }
-    });
+    //     if (!isSticky && scrollTop >= viewportBottom) {
+    //         navbar.addClass("sticky").css('bottom', 'auto').css('top', 0); // Make navbar sticky at the top
+    //         jumber.css('display', 'block');
+    //         isSticky = true;
+    //     } else if (isSticky && scrollTop < viewportBottom) {
+    //         navbar.removeClass("sticky").css('bottom', 0).css('top', 'auto'); // Return navbar to the bottom
+    //         jumber.css('display', 'none');
+    //         isSticky = false;
+    //     }
+    // });
 
     function toggleImage(event) {
         var btn = event.currentTarget;
@@ -37,4 +37,18 @@ $(document).ready(function() {
 
     $('.image-toggle').on('click', toggleImage);
     $('.image-container-right').on('click', hideImage);
+
+    const referenceToggles = document.querySelectorAll('.reference-toggle');
+  
+    referenceToggles.forEach(toggle => {
+        toggle.addEventListener('click', () => {
+            const reference = toggle.nextElementSibling;
+            reference.classList.toggle('active');
+        });
+    });
+
+    window.addEventListener("DOMContentLoaded", () => {
+        const header = new Headroom(document.querySelector("#main-nav"));
+        header.init();
 });
+})
